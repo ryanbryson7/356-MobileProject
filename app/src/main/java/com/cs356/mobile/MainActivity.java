ec440c4e3aef24663eb407ce098b2e45cc5221cf
Andrew Bowden <andrew@ebowden.com>
2021-03-31 14:57:41 -0600

    Added custom top menu

--- app/src/main/java/com/cs356/mobile/MainActivity.java
+++ app/src/main/java/com/cs356/mobile/MainActivity.java
@@ -1,9 +1,12 @@
 package com.cs356.mobile;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.cs356.mobile.ui.calender.CalenderFragment;
 import com.cs356.mobile.ui.event.CreateEventFragment;
@@ -31,6 +34,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         currentFragment = new HomeFragment();
+
+        //hiding default app icon
+        androidx.appcompat.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(false);
+
+        //displaying custom ActionBar
+        View mActionBarView = getLayoutInflater().inflate(R.layout.my_action_bar, null);
+        actionBar.setCustomView(mActionBarView);
+        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+
+        ImageButton notificationButton = findViewById(R.id.notification_button);
+        ImageButton profileButton = findViewById(R.id.profile_button);
         fragmentManager.beginTransaction().add(R.id.fragment_holder, currentFragment).commit();
         navView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -51,6 +66,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        notificationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction().replace(R.id.fragment_holder, new NotificationsFragment()).commit();
+            }
+        });
+
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction().replace(R.id.fragment_holder, new SettingsFragment()).commit();
+            }
+        });
+
+
+
         //TODO: decide if we want to keep the default code
 
         // Passing each menu ID as a set of Ids because each
@@ -63,26 +94,26 @@ public class MainActivity extends AppCompatActivity {
 //        NavigationUI.setupWithNavController(navView, navController);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.top_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.navigation_notifications:
-                fragmentManager.beginTransaction().replace(R.id.fragment_holder, new NotificationsFragment()).commit();
-                return true;
-
-            case R.id.navigation_settings:
-                fragmentManager.beginTransaction().replace(R.id.fragment_holder, new SettingsFragment()).commit();
-                return true;
-
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.top_menu, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case R.id.navigation_notifications:
+//                fragmentManager.beginTransaction().replace(R.id.fragment_holder, new NotificationsFragment()).commit();
+//                return true;
+//
+//            case R.id.navigation_settings:
+//                fragmentManager.beginTransaction().replace(R.id.fragment_holder, new SettingsFragment()).commit();
+//                return true;
+//
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
+//    }
 }
