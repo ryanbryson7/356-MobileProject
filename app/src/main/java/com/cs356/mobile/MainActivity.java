2c0ece5ae55014649ef8d0196419437f561d03b5
Andrew Bowden <andrew@ebowden.com>
2021-04-07 20:56:00 -0600

    Set notification icon to be dynamic

--- app/src/main/java/com/cs356/mobile/MainActivity.java
+++ app/src/main/java/com/cs356/mobile/MainActivity.java
@@ -2,12 +2,15 @@ package com.cs356.mobile;
 
 import android.annotation.SuppressLint;
 import android.app.ActionBar;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.cs356.mobile.model.Data;
 import com.cs356.mobile.ui.calendar.CalendarFragment;
 import com.cs356.mobile.ui.event.CreateEventFragment;
 import com.cs356.mobile.ui.home.HomeFragment;
@@ -16,6 +19,7 @@ import com.cs356.mobile.ui.profile.ProfileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -24,7 +28,9 @@ public class MainActivity extends AppCompatActivity {
     FragmentManager fragmentManager = getSupportFragmentManager();
     Fragment currentFragment;
     TextView pageTitle;
+    ImageButton notificationButton;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @SuppressLint("WrongConstant")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +52,9 @@ public class MainActivity extends AppCompatActivity {
         actionBar.setCustomView(topMenu);
         actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
 
-        ImageButton notificationButton = findViewById(R.id.notification_button);
+        notificationButton = findViewById(R.id.notification_button);
+        setNotificationStatus();
+
         ImageButton profileButton = findViewById(R.id.profile_button);
         pageTitle = findViewById(R.id.page_title);
         fragmentManager.beginTransaction().add(R.id.fragment_holder, currentFragment).commit();
@@ -98,6 +106,16 @@ public class MainActivity extends AppCompatActivity {
         pageTitle.setText(title);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public void setNotificationStatus() {
+        //Set notification icon if there are notifications
+        if (Data.getInstance().getNotifications().size() > 0) {
+            notificationButton.setImageDrawable(getDrawable(R.drawable.ic_notification_active));
+        }
+        else {
+            notificationButton.setImageDrawable(getDrawable(R.drawable.ic_notification_icon));
+        }
+    }
 
 
         //TODO: decide if we want to keep the default code
