7c083e9a5610a90f27cf053176f7b2bc496599ad
Andrew Bowden <andrew@ebowden.com>
2021-04-07 18:10:43 -0600

    working on messages fragment

---
+++ app/src/main/java/com/cs356/mobile/ui/event/EventMessagesFragment.java
@@ -0,0 +1,21 @@
+package com.cs356.mobile.ui.event;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.cs356.mobile.R;
+
+public class EventMessagesFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_event_messages, container, false);
+
+        return view;
+    }
+}
